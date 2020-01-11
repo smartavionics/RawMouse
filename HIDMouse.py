7124cb2477f94c2bb15146d8a23ebd3a83a298aa
Mark Burton <markb@smartavionics.com>
2020-01-11 09:30:29 +0000

    Rename HIDTest -> HIDMouse.

--- HIDMouse.py
+++ HIDMouse.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2020 burtoogle.
-# HIDTest is released under the terms of the AGPLv3 or higher.
+# HIDMouse is released under the terms of the AGPLv3 or higher.
 
 import json
 import sys
@@ -7,12 +7,12 @@ import os
 
 from threading import Thread
 
-#from UM.Controller import Controller
 from UM.Extension import Extension
 from UM.Logger import Logger
+
 from cura.CuraApplication import CuraApplication
 
-from PyQt5.QtCore import QObject, pyqtProperty, pyqtSignal
+from PyQt5.QtCore import QObject
 
 from UM.i18n import i18nCatalog
 catalog = i18nCatalog("cura")
@@ -24,7 +24,7 @@ elif sys.platform == "win32":
 import hid
 del sys.path[-1]
 
-class HIDTest(Extension, QObject,):
+class HIDMouse(Extension, QObject,):
     def __init__(self, parent = None):
         QObject.__init__(self, parent)
         Extension.__init__(self)
@@ -32,18 +32,19 @@ class HIDTest(Extension, QObject,):
         self._application = CuraApplication.getInstance()
         self._camera_tool = self._application.getController().getCameraTool()
 
-        self.setMenuName(catalog.i18nc("@item:inmenu", "HIDTest"))
+        self.setMenuName(catalog.i18nc("@item:inmenu", "HIDMouse"))
         self.addMenuItem(catalog.i18nc("@item:inmenu", "Reload"), self._reload)
         self.addMenuItem(catalog.i18nc("@item:inmenu", "Restart"), self._restart)
         self.addMenuItem(catalog.i18nc("@item:inmenu", "Stop"), self._stop)
 
+        self._buttons = 0
         self._running = False
         self._runner = None
         self._reload()
         self._start()
 
     def _reload(self):
-        with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "hid.json"), "r", encoding = "utf-8") as f:
+        with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "hidmouse.json"), "r", encoding = "utf-8") as f:
             self._config = json.load(f)
 
     def _restart(self):
@@ -56,7 +57,8 @@ class HIDTest(Extension, QObject,):
             for known_dev in self._config["devices"]:
                 if hid_dev["vendor_id"] == int(known_dev[0], base = 16) and hid_dev["product_id"] == int(known_dev[1], base = 16):
                     self._hid_dev = hid_dev
-                    self._hid_profile = known_dev[2]
+                    self._hid_profile_name = known_dev[2]
+                    self._hid_profile = self._config["profiles"][self._hid_profile_name]
                     break
             if self._hid_dev is not None:
                 break
@@ -99,31 +101,44 @@ class HIDTest(Extension, QObject,):
         self._runner = None
 
     def _decodeHIDEvent(self, buf):
-        if self._hid_profile == "spacemouse":
-            self._decodeSpacemouseAxisEvent(buf)
+        if self._hid_profile_name == "spacemouse":
+            self._decodeSpacemouseEvent(buf)
         else:
-            Logger.log("d", "Unknown HID event: profile= %s, code = %x, len = %d", self._hid_profile, buf[0], len(buf))
+            Logger.log("d", "Unknown HID event: profile = %s, code = %x, len = %d", self._hid_profile_name, buf[0], len(buf))
 
-    def _decodeSpacemouseAxisEvent(self, buf):
+    def _decodeSpacemouseEvent(self, buf):
         if len(buf) == 7 and (buf[0] == 1 or buf[0] == 2):
+            hid_profile_axes = self._hid_profile["axes"]
             for a in range(0, 3):
                 val = buf[2 * a + 1] | buf[2 * a + 2] << 8
                 if val & 0x8000:
                     val = val - 0x10000
-                val = val / 350.0
-                if abs(val) > 0.01:
-                    self._spacemouseAxisEvent((buf[0] - 1) * 3 + a, val)
+                axis = (buf[0] - 1) * 3 + a
+                axis_config = hid_profile_axes[axis]
+                val = val / 350.0 * axis_config["scale"] + axis_config["offset"]
+                self._spacemouseAxisEvent(axis, val)
         elif len(buf) == 13 and buf[0] == 1:
+            hid_profile_axes = self._hid_profile["axes"]
             for a in range(0, 6):
                 val = buf[2 * a + 1] | buf[2 * a + 2] << 8
                 if val & 0x8000:
                     val = val - 0x10000
-                val = val / 350.0
-                if abs(val) > 0.01:
-                    self._spacemouseAxisEvent(a, val)
+                axis_config = hid_profile_axes[a]
+                val = val / 350.0 * axis_config["scale"] + axis_config["offset"]
+                self._spacemouseAxisEvent(a, val)
+        elif len(buf) >= 3 and buf[0] == 3:
+            buttons = buf[1] | buf[2] << 8
+            for b in range(0, 16):
+                mask = 1 << b
+                if ((buttons & mask) != (self._buttons & mask)):
+                    self._spacemouseButtonEvent(b, (buttons & mask) >> b)
+            self._buttons = buttons
         else:
             Logger.log("d", "Unknown spacemouse event: code = %x, len = %d", buf[0], len(buf))
 
     def _spacemouseAxisEvent(self, axis, val):
         Logger.log("d", "axis[%d] = %f", axis, val)
+
+    def _spacemouseButtonEvent(self, button, val):
+        Logger.log("d", "button[%d] = %f", button, val)
 
--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 # Copyright (c) 2020 burtoogle.
-# HIDTest is released under the terms of the AGPLv3 or higher.
+# HIDMouse is released under the terms of the AGPLv3 or higher.
 
-from . import HIDTest
+from . import HIDMouse
 
 def getMetaData():
     return { }
 
 def register(app):
-    return { "extension": HIDTest.HIDTest() }
+    return { "extension": HIDMouse.HIDMouse() }
