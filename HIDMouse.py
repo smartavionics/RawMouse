8774de6dc4291608ed2331ce7820c3176cad757a
Mark Burton <markb@smartavionics.com>
2020-01-13 11:46:28 +0000

    Added _initTargetValues() and _processTargetValues().

--- HIDMouse.py
+++ HIDMouse.py
@@ -52,6 +52,11 @@ class HIDMouse(Extension, QObject,):
         self._reload()
         self._start()
 
+    def _restart(self):
+        self._stop()
+        self._reload()
+        self._start()
+
     def _reload(self):
         self._config = {}
         try:
@@ -84,11 +89,6 @@ class HIDMouse(Extension, QObject,):
                 self._axis_target.append("")
             self._axis_value.append(0.0)
             Logger.log("d", "%s: axis %d, scale = %f, threshold = %f, offset = %f, target = %s", self._hid_profile_name, i, self._axis_scale[i], self._axis_threshold[i], self._axis_offset[i], self._axis_target[i])
-
-    def _restart(self):
-        self._stop()
-        self._reload()
-        self._start()
 
     def _start(self):
         self._hid_dev = None
@@ -150,6 +150,27 @@ class HIDMouse(Extension, QObject,):
         self._running = False
         self._runner = None
 
+    def _initTargetValues(self):
+        self._target_values = {
+            "movx": 0.0,
+            "movy": 0.0,
+            "rotx": 0.0,
+            "roty": 0.0,
+            "zoom": 0.0,
+            "resetview": None
+        }
+
+    def _processTargetValues(self):
+        if self._target_values["resetview"]:
+            self._resetView(self._target_values["resetview"])
+        else:
+            if self._target_values["movx"] != 0.0 or self._target_values["movy"] != 0.0:
+                self._camera_tool._moveCamera(MouseEvent(MouseEvent.MouseMoveEvent, self._target_values["movx"], self._target_values["movy"], 0, 0))
+            if self._target_values["rotx"] != 0 or self._target_values["roty"] != 0:
+                self._camera_tool._rotateCamera(self._target_values["rotx"], self._target_values["roty"])
+            if self._target_values["zoom"] != 0:
+                self._camera_tool._zoomCamera(self._target_values["zoom"])
+
     def _decodeSpacemouseEvent(self, buf):
         scale = 1.0 / 350.0
         if len(buf) == 7 and (buf[0] == 1 or buf[0] == 2):
@@ -180,57 +201,31 @@ class HIDMouse(Extension, QObject,):
     def _spacemouseAxisEvent(self, vals):
         Logger.log("d", "Axes [%f,%f,%f,%f,%f,%f]", vals[0], vals[1], vals[2], vals[3], vals[4], vals[5])
         if self._camera_tool:
-            value = {
-                "movx": 0.0,
-                "movy": 0.0,
-                "rotx": 0.0,
-                "roty": 0.0,
-                "zoom": 0.0
-            }
-            for i in range(0, 5):
+            self._initTargetValues()
+            for i in range(0, 6):
                 if abs(vals[i]) > self._axis_threshold[i]:
-                    value[self._axis_target[i]] = vals[i]
-            if value["movx"] != 0.0 or value["movy"] != 0.0:
-                self._camera_tool._moveCamera(MouseEvent(MouseEvent.MouseMoveEvent, value["movx"], value["movy"], 0, 0))
-            if value["rotx"] != 0 or value["roty"] != 0:
-                self._camera_tool._rotateCamera(value["rotx"], value["roty"])
-            if value["zoom"] != 0:
-                self._camera_tool._zoomCamera(value["zoom"])
+                    self._target_values[self._axis_target[i]] = vals[i]
+            self._processTargetValues();
 
     def _spacemouseButtonEvent(self, button, val):
         Logger.log("d", "button[%d] = %f", button, val)
 
     def _decodeTiltpadEvent(self, buf):
         if self._camera_tool:
-            value = {
-                "movx": 0.0,
-                "movy": 0.0,
-                "rotx": 0.0,
-                "roty": 0.0,
-                "zoom": 0.0,
-                "resetview": None
-            }
+            self._initTargetValues()
             scale = 1.0
             #tilt
             for a in range(0, 2):
                 val = (buf[a] - 127) * scale * self._axis_scale[a] + self._axis_offset[a]
                 if abs(val) > self._axis_threshold[a]:
-                    value[self._axis_target[a]] = val
+                    self._target_values[self._axis_target[a]] = val
             buttons = buf[3] & 0x7f
             if buttons != 0:
                 button_defs = self._hid_profile["buttons"]
                 for b in button_defs:
                     if buttons == int(b, base = 16):
-                        value[button_defs[b]["target"]] = button_defs[b]["value"]
-            if value["resetview"]:
-                self._resetView(value["resetview"])
-            else:
-                if value["movx"] != 0.0 or value["movy"] != 0.0:
-                    self._camera_tool._moveCamera(MouseEvent(MouseEvent.MouseMoveEvent, value["movx"], value["movy"], 0, 0))
-                if value["rotx"] != 0 or value["roty"] != 0:
-                    self._camera_tool._rotateCamera(value["rotx"], value["roty"])
-                if value["zoom"] != 0:
-                    self._camera_tool._zoomCamera(value["zoom"])
+                        self._target_values[button_defs[b]["target"]] = button_defs[b]["value"]
+            self._processTargetValues()
 
     def _decodeUnknownEvent(self, buf):
         Logger.log("d", "Unknown event: len = %d [0] = %x", len(buf), buf[0])
