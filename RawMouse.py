596b3e59fcae44bcf77eb70251e49718695c73aa
Mark Burton <markb@smartavionics.com>
2020-01-26 11:15:36 +0000

    Use +=.

--- RawMouse.py
+++ RawMouse.py
@@ -336,11 +336,11 @@ class RawMouse(Extension, QObject,):
             if self._hid_dev:
                 message = "Manufacturer: " + self._hid_dev["manufacturer_string"] + "\nProduct: " + self._hid_dev["product_string"] + "\nProfile: " + self._hid_profile_name;
             if self._battery_level is not None:
-                message = message + "\nBattery level: " + str(self._battery_level) + "%"
+                message += "\nBattery level: " + str(self._battery_level) + "%"
             if self._hid_profile:
-                message = message + "\nAxes:"
+                message += "\nAxes:"
                 for i in range(0, len(self._axis_scale)):
-                    message = message + "\n [" + str(i) + "] scale " + str(self._axis_scale[i]) + " threshold " + str(self._axis_threshold[i]) + " offset " + str(self._axis_offset[i]) + " -> " + self._axis_target[i]
+                    message += "\n [" + str(i) + "] scale " + str(self._axis_scale[i]) + " threshold " + str(self._axis_threshold[i]) + " offset " + str(self._axis_offset[i]) + " -> " + self._axis_target[i]
             self._showMessage(message)
         except Exception as e:
             Logger.log("e", "Exception while showing device information: %s", e)
